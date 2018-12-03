c396de881e8e7918401a20f3989aa6764f25e95e
milos-m <milos.m@toptal.com>
2018-12-03 08:51:19 +0100

    Initial project setup, playing around with angular.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AboutComponent } from './components/about/about.component';
+import { NotesComponent } from './components/notes/notes.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'notes', pathMatch: 'full' },
+  { path: 'notes', component: NotesComponent },
+  { path: 'about', component: AboutComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
